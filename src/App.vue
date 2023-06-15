ce43bec5d2be7a3367c23b773c24a322493900fd
gnemek20 <gnemek20@gmail.com>
2023-06-15 23:39:33 +0900

    필요없는 페이지 제거 및 웹 전체의 margin, padding, 드래그 제거

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
@@ -16,17 +12,12 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 </style>
